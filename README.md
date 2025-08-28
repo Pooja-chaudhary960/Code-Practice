2e613b9a7043931a8292cf140fcd106c34c28245
Pooja-chaudhary960 <pujadahit52@gmail.com>
2025-08-28 11:52:37 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# JavaScript 
+- Programming language
+- High level, interpreted, multi-paradigm, dynamic
+- Built on C++
+- Node.js is the runtime of JavaScript.
+- Node.js used to build APIs
+- Used to build frontend web pages (add interactivity)
---
+++ main.js
@@ -0,0 +1,29 @@
+// Print
+console.log("hello world");
+console.error("This is Error");
+console.warn("This is Warn");
+
+// Variable : It is a memory element
+var age = 20;
+var name = "Ram";
+var isMale = false;
+
+console.log(age);
+
+var age = 30;
+console.log(age);
+
+// Note: NEVER USE var for variable declaration
+// Always use const, unless let is required.
+
+
+// let, const
+let phone = 983737766;
+phone = 9876648383;
+
+console.log(phone);
+
+const address = "Dang";
+address="Lamahi";
+
+console.log(address);
